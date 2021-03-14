1bd4b93d3d59ac18ef6520d1b363564af5c88ccf
uyennguyen16900 <uyennguyen16900@gmail.com>
2021-03-14 16:10:22 -0700

    handled connections and messages

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/uyennguyen16900/chat-rooms-golang
 
 go 1.15
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+var upgrader = websocket.Upgrader{}
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	// register new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// grab the next message from the broadcast channel
+		msg := <-broadcast
+		// send it to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+
+	}
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/ws", handleConnections)
+
+	// start listening for incoming chat messages
+	go handleMessages()
+
+	// start the server on localhost port 8000 and log any errors
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
