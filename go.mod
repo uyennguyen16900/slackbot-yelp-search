293eaf5802eb84aa2da113e9f4872c030d6e7bac
uyennguyen16900 <uyennguyen16900@gmail.com>
2021-03-15 15:31:35 -0700

    invited the bot to the channel

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/uyennguyen16900/chat-rooms-golang
 
 go 1.15
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/slack-go/slack v0.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,75 +2,30 @@ package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/slack-go/slack"
 )
 
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan Message)
-
-var upgrader = websocket.Upgrader{}
-
-type Message struct {
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Message  string `json:"message"`
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// upgrade initial GET request to a websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ws.Close()
-
-	// register new client
-	clients[ws] = true
-
-	for {
-		var msg Message
-		err := ws.ReadJSON(&msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
-			break
-		}
-		broadcast <- msg
-	}
-}
-
-func handleMessages() {
-	for {
-		// grab the next message from the broadcast channel
-		msg := <-broadcast
-		// send it to every client that is currently connected
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
-
-	}
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
+	OAUTH_TOKEN := "xoxb-1303899646098-1846760895863-7I0u8xonCO69oxKOTlVC7rzJ"
+	CHANNEL_ID := "C01R8CRBX1B"
 
-	http.HandleFunc("/ws", handleConnections)
-
-	// start listening for incoming chat messages
-	go handleMessages()
-
-	// start the server on localhost port 8000 and log any errors
-	log.Println("http server started on :8000")
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	api := slack.New(OAUTH_TOKEN)
+	attachment := slack.Attachment{
+		Pretext: "Pretext",
+		Text:    "Hello from GolangDocs",
 	}
+
+	channelID, timestamp, err := api.PostMessage(
+		CHANNEL_ID,
+		slack.MsgOptionText("This is the main message", false),
+		slack.MsgOptionAttachments(attachment),
+		slack.MsgOptionAsUser(true),
+	)
+
+	if err != nil {
+		log.Fatalf("%s\n", err)
+	}
+
+	log.Printf("Message successfullt sent to Channel %s at %s\n", channelID, timestamp)
 }
